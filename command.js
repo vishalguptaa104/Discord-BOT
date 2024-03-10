e9c6ee5068291ff0274e91672de340c56ddb8739
vishalguptaa104 <119746913+vishalguptaa104@users.noreply.github.com>
2024-03-10 10:05:45 +0000

    commit

--- command.js
+++ command.js
@@ -1,20 +1,20 @@
-import { REST, Routes } from 'discord.js';
+// import { REST, Routes } from 'discord.js';
 
-const commands = [
-    {
-      name: 'ping',
-      description: 'Replies with Pong!',
-    },
-  ];
+// const commands = [
+//     {
+//       name: 'ping',
+//       description: 'Replies with Pong!',
+//     },
+//   ];
 
-  const rest = new REST({ version: '10' }).setToken("MTIxNjA1MzIzMDg4NzUwNjExMg.G_M2ah.TK1FlvCFcBoOQi1WJSkT3WvFVYtxYA6J263KTc");
+//   const rest = new REST({ version: '10' }).setToken("TOKEN");
 
-  try {
-    console.log('Started refreshing application (/) commands.');
+//   try {
+//     console.log('Started refreshing application (/) commands.');
   
-    await rest.put(Routes.applicationCommands('1216053230887506112'), { body: commands });
+//     await rest.put(Routes.applicationCommands('CLIENT_ID'), { body: commands });
   
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
+//     console.log('Successfully reloaded application (/) commands.');
+//   } catch (error) {
+//     console.error(error);
+//   }
--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-import { Client, GatewayIntentBits } from "discord.js";
+// import { Client, GatewayIntentBits } from "discord.js";
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
+// const client = new Client({
+//   intents: [
+//     GatewayIntentBits.Guilds,
+//     GatewayIntentBits.GuildMessages,
+//     GatewayIntentBits.MessageContent,
+//   ],
+// });
 
-client.on("messageCreate", (message) => {
-  if(message.author.bot) return;
-  else{
-  message.reply({
-    content: "Hi This is bot"
-  })
-}
-});
+// client.on("messageCreate", (message) => {
+//   if(message.author.bot) return;
+//   else{
+//   message.reply({
+//     content: "Hi This is bot"
+//   })
+// }
+// });
 
-client.on('interactionCreate', interaction => {
-  // console.log(interaction);
-  interaction.reply("Pong!")
-})
+// client.on('interactionCreate', interaction => {
+//   // console.log(interaction);
+//   interaction.reply("Pong!")
+// })
 
-// client.login(
-//   ""
-// );
+// // client.login(
+// //   ""
+// // );
